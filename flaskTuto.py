2287c451036c1d4f53fa51007285cfce38bb341d
raphaelchalicarne <raphael.chalicarne@outlook.com>
2019-12-12 11:16:39 +0100

    Variable rules

--- flaskTuto.py
+++ flaskTuto.py
@@ -7,7 +7,10 @@ Created on Wed Dec 11 16:30:52 2019
 """
 
 from flask import Flask
+from flask import escape
 app = Flask(__name__)
+
+# %% Routing
 
 @app.route('/')
 def index():
@@ -16,3 +19,19 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+# %% Variable rules
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
