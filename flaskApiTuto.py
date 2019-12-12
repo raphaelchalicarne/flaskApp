59afe7a59210989bc1a8b9872e3ae0d10c506136
raphaelchalicarne <raphael.chalicarne@outlook.com>
2019-12-12 17:18:08 +0100

    Tutoriel of a Flask API

---
+++ flaskApiTuto.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 12 17:16:48 2019
+
+@author: Raphaël Chalicarne
+@github : https://github.com/raphaelchalicarne
+Tutoriel from https://www.fernandomc.com/posts/your-first-flask-api/
+"""
+
